b2e58af75a352d33bdfea00b5905e0e888aeca92
Yoosun Jang <yoosunjang0915@gmail.com>
2024-07-06 14:17:20 +0900

    rails watch list

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,3 +5,13 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin "bootstrap", to: "bootstrap.min.js", preload: true
+pin "@popperjs/core", to: "popper.js", preload: true
+
+# config/importmap.rb
+
+# replace these lines:
+# pin "bootstrap" # @5.3.2
+# pin "@popperjs/core", to: "@popperjs--core.js" # @2.11.8
+
+# with this:
